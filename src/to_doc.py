7b95b3d09f2438e76b45e98575dfbf638aaca8b8
CAMILLE <camille@CAMILLEcBookNeo.lan>
2026-08-20 20:22:43 +0200

    Change csv to jsonl and clean.

--- src/to_doc.py
+++ src/to_doc.py
@@ -1,33 +1,103 @@
-import json
+import csv, json
+import os
+from pathlib import Path
+import re
+import unicodedata
 
-import pandas as pd
 
-df = pd.read_csv("data/SkincareAddiction.csv")
+def wash_post(post: str):
 
-# Create documents for LangChain
-documents = []
+    # --- Unicode normalization (safe, no escape corruption)
+    post = unicodedata.normalize("NFKC", post)
 
-for idx, row in df.iterrows():
-    # skip documents without text (nan)
-    if isinstance(row["selftext"], str):
-        text = f"""
-        Title:
-        {row["title"]}
+    # Replace reddit unicode
+    post = (
+        post.replace("\u2018", "'")
+        .replace("\u2019", "'")
+        .replace("\u2022", "-")
+        .replace("\u201c", '"')
+        .replace("\u201d", '"')
+        .replace("\u00a0", " ")
+        .replace("\u200b", "")
+        .replace("&#x200B;", "")
+    )
 
-        Content:
-        {row["selftext"]}
-        """
+    # Collapse multiple newlines → single newline
+    post = re.sub(r"\n\s*\n+", "\n", post)
 
-        documents.append(
-            {
-                "id": row["id"],
-                "text": text,
-                "metadata": {"score": row["score"], "comments": row["num_comments"]},
+    # Strip markdown (or convert to plain text)
+
+    # take urls out of post
+    post = re.sub(r"https?://\S+", "[URL]", post)
+
+    return post
+
+
+def csv_to_jsonl(csv_file: str):
+    """
+    Convert csv file to jsonl for LangChain.
+    """
+
+    # get filename without extension or folder
+    filename = Path(csv_file)
+    out_name = filename.stem  # filename without extension
+    _, source = str(out_name).split("_", 1)
+
+    # Create documents for LangChain
+    docs = []
+
+    with open(csv_file) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+
+            post = row["post"]
+
+            # Remove repeated boilerplate
+            boilerplate = [
+                "Today is Casual Friday",
+                "If you're new to SkincareAddiction: welcome",
+                "Asian Beauty isn’t ALL about skincare",
+                "Post all of your deals, memes, gifs, hauls, sheet mask selfies, and other fluff.", 
+                "Hello and welcome to the Daily Help Thread", 
+                "Have a rant about your routine or beauty products", 
+                "It’s the Weekly Random Chat Post! ",
+                "Frustrated and need to rant",
+                "seen several comments about the same old same", 
+                "kick off the month with a thread to keep purchases in check", 
+                "Welcome to our WITW Shopping Guide series",
+                "discuss the anti-haul, where more",
+            ]
+
+            # if post contains daily post, skip appending this doc
+            if any(b in post for b in boilerplate) or len(post) < 2:
+                continue
+
+            # Take urls out of post and add to "urls "
+            urls = re.findall(r"https?://\S+", post)
+
+            clean_post = wash_post(post)
+            clean_title = wash_post(row["title"])
+
+            doc = {
+                "id": f"{clean_title[:20]}_{row['time_utc']}",
+                "title": clean_title,
+                "post": clean_post,
+                "time_utc": float(row["time_utc"]),
+                "upvote": float(row["upvote"]),
+                "num_comments": int(row["num_comments"]),
+                "urls": urls,
+                "source": "reddit_" + source,
             }
-        )
+            docs.append(doc)
 
+    out_path = Path("data/clean") / f"{out_name}.jsonl"
+    with open(out_path, "w", encoding="utf-8") as out:
+        for d in docs:
+            out.write(json.dumps(d, ensure_ascii=False) + "\n")
 
-# Write each document to file
-for i, doc in enumerate(documents):
-    with open("data/processed/Skincare" + str(i), "w") as f:
-        json.dump(doc, f, indent=4)
+obj = os.scandir("data/raw")
+for entry in obj:
+    if entry.is_file():
+        csv_to_jsonl(entry)
+
+    
